a55cd8e896130492ac5a622bc1f3a1727910a7ca
jaeseokim <48559454+JaeSeoKim@users.noreply.github.com>
2020-03-17 21:47:57 +0900

    #3.17 지도 이동 이벤트 추가

--- src/components/MaskStoreList.js
+++ src/components/MaskStoreList.js
@@ -3,39 +3,66 @@ import { Collapse, Card, Tag } from "antd";
 import { useSelector } from "react-redux";
 
 const { Panel } = Collapse;
+const { kakao } = window;
+
+var kakaoMap = null;
 
 const MaskStoreList = () => {
 
-  const { storeList } = useSelector(state => ({
+  const { storeList, map } = useSelector(state => ({
     storeList: state.maskMap.storeList,
+    map: state.maskMap.map
   }));
+
+  kakaoMap = map;
+
+  const onclickHandle = (lat, lng) => {
+    kakaoMap.panTo(new kakao.maps.LatLng(lat, lng));
+  };
+
+  const scrollUpDelay = 1;
+  const scrollUpSpeed = 60;
+  
+  const scrollUp = () => {
+    console.log("scrollUp");
+    if (document.documentElement.scrollTop < 1) {
+      return;
+    }
+    document.documentElement.scrollTop = document.documentElement.scrollTop - scrollUpSpeed;
+    setTimeout(() => {
+      scrollUp();
+    }, scrollUpDelay);
+  }
 
   return (
     <div className="more_Info">
       <Collapse style={{ maxWidth: 500, margin: "auto", marginBottom: 13, marginTop: 15 }}>
         <Panel header={"지도의 기준 현재 구매 가능한 마스크 판매처"} key="maskMap">
-          <h3 style={{textAlign:"left"}}>
+          <h3 style={{ textAlign: "left" }}>
             <p>지도의 중심 기준 1000M 반경의 판매처를 가지고 사용합니다.</p>
-            <p>현재 구매 가능한 구매처만 목록화 했습니다.</p>
-            <p>해당 카드를 클릭 하시면 판매처 주소를 확인 할 수 있습니다.</p>
+            <p>현재 1개 이상 재고를 소유한 판매처만 목록화 했습니다.</p>
+            <p>해당 카드를 클릭 하시면 지도가 해당 판매처를 가리키게 됩니다!</p>
           </h3>
           {storeList.map((value, index) => (
-            typeof value.remain_stat !== "undefined"  && value.remain_stat !== ""  && value.remain_stat !== null && value.remain_stat !== "break" && value.remain_stat !== "empty" &&
-              <a href={"https://map.kakao.com/link/map/" + value.name + "," + value.lat + "," + value.lng} target="_blank" key={value.code}>
-                <Card style={{ maxWidth: 500, margin: "auto", marginBottom: 13 }}
-                  title={value.name} >
-                  <div style={{ textAlign: "left" }}>
-                    <p>주소 : {value.addr}</p>
-                    <p>마지막 업데이트 : {value.created_at}</p>
-                    <p>입고 시간 : {value.stock_at}</p>
-                    <p>
-                      {value.remain_stat === "plenty" && <Tag color="#87d068">100개 이상</Tag>}
-                      {value.remain_stat === "some" && <Tag color="#2db7f5">30개 이상 100개 미만</Tag>}
-                      {value.remain_stat === "few" && <Tag color="#f50">30개 미만</Tag>}
-                    </p>
-                  </div>
-                </Card>
-              </a>
+            typeof value.remain_stat !== "undefined" && value.remain_stat !== "" && value.remain_stat !== null && value.remain_stat !== "break" && value.remain_stat !== "empty" &&
+            <div key={value.code}>
+              <Card hoverable onClick={() => {
+                onclickHandle(value.lat, value.lng);
+                scrollUp();
+              }} style={{ maxWidth: 500, margin: "auto", marginBottom: 13 }}
+                title={value.name} >
+                <div style={{ textAlign: "left" }}>
+                  <p>주소 : {value.addr}</p>
+                  <p>마지막 업데이트 : {value.created_at}</p>
+                  <p>입고 시간 : {value.stock_at}</p>
+                  <p>
+                    {value.remain_stat === "plenty" && <Tag color="#87d068">100개 이상</Tag>}
+                    {value.remain_stat === "some" && <Tag color="#2db7f5">30개 이상 100개 미만</Tag>}
+                    {value.remain_stat === "few" && <Tag color="#f50">30개 미만</Tag>}
+                  </p>
+                </div>
+              </Card>
+            </div>
           ))}
         </Panel>
       </Collapse>
